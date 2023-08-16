a31c2e2a38bce92e5973b38f87857dd62dc4a259
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-08-16 11:02:54 -0500

    Server settings change

--- src/knex.js
+++ src/knex.js
@@ -2,17 +2,18 @@ const knex = require("knex");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const HOST = process.env.HOST
+const PORT = process.env.PORT
 const USERNAME = process.env.USERNAME
 const PASSWORD = process.env.PASSWORD
 const DATABASE = process.env.DATABASE
-
 
 module.exports = function (app) {
   const db = require("knex")({
     client: "mysql2",
     connection: {
-      host: "localhost",
-      port: 3306,
+      host: HOST,
+      port: PORT,
       user: USERNAME,
       password: PASSWORD,
       database: DATABASE,
