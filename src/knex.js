28edfbdf73beb2a46f7ebbb64c9e2864399f1d68
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-09-09 19:49:56 -0500

    Added knex-planetscale adapter

--- src/knex.js
+++ src/knex.js
@@ -1,3 +1,4 @@
+/*
 const knex = require("knex");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -14,6 +15,30 @@ module.exports = function (app) {
     connection: {
       host: HOST,
       port: PORT,
+      user: USERNAME,
+      password: PASSWORD,
+      database: DATABASE,
+    },
+  });
+
+  app.set("knexClient", db);
+};
+*/
+
+import { Client_PSDB } from "knex-planetscale";
+const knex = require("knex");
+const dotenv = require("dotenv");
+
+const HOST = process.env.HOST;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
+
+module.exports = function (app) {
+  const db = require("knex")({
+    client: Client_PSDB,
+    connection: {
+      host: HOST,
       user: USERNAME,
       password: PASSWORD,
       database: DATABASE,
