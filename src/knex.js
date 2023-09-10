38d2f540c5c4c6ec9ee9d4132c61ddc1eb04a648
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-09-09 19:53:49 -0500

    Undo knex-planetscale

--- src/knex.js
+++ src/knex.js
@@ -1,13 +1,12 @@
-/*
 const knex = require("knex");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const HOST = process.env.HOST
-const PORT = process.env.PORT
-const USERNAME = process.env.USERNAME
-const PASSWORD = process.env.PASSWORD
-const DATABASE = process.env.DATABASE
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
 
 module.exports = function (app) {
   const db = require("knex")({
@@ -15,30 +14,6 @@ module.exports = function (app) {
     connection: {
       host: HOST,
       port: PORT,
-      user: USERNAME,
-      password: PASSWORD,
-      database: DATABASE,
-    },
-  });
-
-  app.set("knexClient", db);
-};
-*/
-
-import { Client_PSDB } from "knex-planetscale";
-const knex = require("knex");
-const dotenv = require("dotenv");
-
-const HOST = process.env.HOST;
-const USERNAME = process.env.USERNAME;
-const PASSWORD = process.env.PASSWORD;
-const DATABASE = process.env.DATABASE;
-
-module.exports = function (app) {
-  const db = require("knex")({
-    client: Client_PSDB,
-    connection: {
-      host: HOST,
       user: USERNAME,
       password: PASSWORD,
       database: DATABASE,
